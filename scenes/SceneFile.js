5fc74074a04eb515e5a7f0f5ef842a9c95f9c0e4
Achal Srinivasan <achal@achal.me>
2020-07-01 13:55:30 -0700

    Update imports

--- scenes/SceneFile.js
+++ scenes/SceneFile.js
@@ -3,7 +3,7 @@ import * as Strings from "~/common/strings";
 import * as Constants from "~/common/constants";
 import * as Fixtures from "~/common/fixtures";
 import * as System from "~/components/system";
-import * as SVG from "~/components/system/svg";
+import * as SVG from "~/components/system/util/svg";
 
 import { css } from "@emotion/react";
 
--- scenes/SceneWallet.js
+++ scenes/SceneWallet.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as Strings from "~/common/strings";
 import * as Constants from "~/common/constants";
-import * as SVG from "~/components/system/svg";
+import * as SVG from "~/components/system/util/svg";
 import * as Fixtures from "~/common/fixtures";
 import * as System from "~/components/system";
 import * as SchemaTable from "~/common/schema-table";
